ad3b4c79677328d8333cc6bd91e059a61b28218a
toni.dermawan <toni_d_y@yahoo.co.id>
2012-08-15 14:19:51 +0700

    Change welcome msg again

--- mvc3web/Controllers/HomeController.cs
+++ mvc3web/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace mvc3web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to ASP.NET MVC! rev3";
 
             return View();
         }
